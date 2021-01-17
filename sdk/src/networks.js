11eb6bf3687693e452a59414727fa1c6dbc89bf8
realdao001 <realdao@protonmail.com>
2021-01-17 10:59:03 +0800

    sdk: add testnet config

--- sdk/src/networks.js
+++ sdk/src/networks.js
@@ -4,11 +4,6 @@ module.exports = {
     chainId: 0x539,
     orchestrator: '0xfe57175001DAA3BacB220dBe30036241E20F1FA0',
   },
-  test: {
-    provider: 'ws://139.180.193.123:8545',
-    chainId: 0x539,
-    orchestrator: '0x1d736CBAB67422a524E6923A2e4f47C2Ae891335',
-  },
   kovan: {
     provider: 'https://kovan.infura.io/v3/d3f8f9c2141b4561b6c7f23a34466d7c',
     chainId: 42,
@@ -19,8 +14,13 @@ module.exports = {
     chainId: 3,
     orchestrator: '0x297344B27D52abAe0f30AFE947ddAd60d425F40d',
   },
-  live: {
-    provider: 'ws://localhost:8545',
+  testnet: {
+    provider: 'wss://ws-testnet.hecochain.com',
+    chainId: 0x539,
+    orchestrator: '0x1d736CBAB67422a524E6923A2e4f47C2Ae891335',
+  },
+  mainnet: {
+    provider: 'wss://ws-mainnet.hecochain.com',
     chainId: 1,
     orchestrator: '0x297344B27D52abAe0f30AFE947ddAd60d425F40d',
   },
