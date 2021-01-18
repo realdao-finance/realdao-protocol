8c063580180b1bebab4045be6944c9c3d2355d3a
realdao001 <realdao@protonmail.com>
2021-01-19 00:00:33 +0800

    tools: support batch transfer of fake lp tokens

--- tools/testnet-faucet/index.js
+++ tools/testnet-faucet/index.js
@@ -1,7 +1,7 @@
-const ABI_EIP20 = require('./EIP20Interface.json')
 const Web3 = require('web3')
 const fastify = require('fastify')
 const path = require('path')
+const { RealDAO } = require('../../sdk')
 const env = require('../../.env.js')
 const { advanceBlock } = require('../../test/util/advance')
 
@@ -19,11 +19,30 @@ function ok(reply, result) {
   })
 }
 
+function transferBatch(tokens, sender, recipient, amount) {
+  const transactions = tokens.map((token) => {
+    return token.transfer(recipient, amount).send({ from: sender, gas: 4000000 })
+  })
+  return Promise.all(transactions)
+}
+
 async function main(argv) {
   const addr = argv[2]
   const web3 = new Web3(env.networks[env.current].provider)
   global.web3 = web3
-  const token = new web3.eth.Contract(ABI_EIP20, addr)
+  const realDAO = new RealDAO({
+    Web3,
+    env: env.current,
+    provider: env.networks[env.current].provider,
+    orchestrator: env.networks[env.current].orchestrator,
+  })
+  await realDAO.loadDistributor()
+  const pools = await realDAO.distributor().getAllPools().call()
+  console.log('pools:', pools)
+  let fakeLPTokens = []
+  for (const pool of pools) {
+    fakeLPTokens.push(realDAO.erc20Token(pool.tokenAddr))
+  }
   const accounts = await web3.eth.getAccounts()
   const admin = accounts[0]
   const userStats = new Map()
@@ -56,9 +75,7 @@ async function main(argv) {
     if (!web3.utils.isAddress(recipient)) {
       return fail(reply, 'Not an address')
     }
-    token.methods
-      .transfer(recipient, '1000000000000000000')
-      .send({ from: admin, gas: 4000000 })
+    transferBatch(fakeLPTokens, admin, recipient, BigInt(1e21).toString())
       .then((result) => {
         addCount(recipient)
         ok(reply, result.transactionHash)
